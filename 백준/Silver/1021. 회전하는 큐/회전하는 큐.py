ab73a448ed8b6cee3fb17b57596dc18288a3c54a
허재원 <jaewonheo666@gmail.com>
2023-08-25 17:54:29 +0900

    [Silver III] Title: 회전하는 큐, Time: 96 ms, Memory: 34272 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+n,m = map(int,input().split())
+data = deque([i for i in range(1,n+1)])
+
+index = list(map(int,input().split()))
+
+count = 0
+for i in index:
+    while 1:
+        if data[0] == i:
+            data.popleft()
+            break
+        else:
+            if data.index(i) <= len(data)//2:
+                data.rotate(-1)
+                count += 1
+            else:
+                data.rotate(1)
+                count += 1
+
+print(count)
