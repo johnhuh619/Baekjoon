ec54ad1221b314d2cfc685e297c34b799faa2db1
허재원 <jaewonheo666@gmail.com>
2023-09-21 18:58:49 +0900

    [Silver I] Title: 스타트링크, Time: 452 ms, Memory: 70812 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+f, s, g, u, d = map(int,input().split())
+q = deque()
+visited = [-1]*(f+1)
+def bfs(start, goal):
+    q.append(start)
+    visited[start] = 0
+    while q:
+        x = q.popleft()
+
+        if x == goal:
+            return visited[x]
+        for nx in (x+u,x-d):
+            if 0< nx <= f and visited[nx] == -1:
+                q.append(nx)
+                visited[nx] = visited[x] + 1
+    return "use the stairs"
+
+print(bfs(s,g))
