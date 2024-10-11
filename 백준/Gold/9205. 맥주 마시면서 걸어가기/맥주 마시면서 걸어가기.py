03022a34c82589ee582be34f1f79de8f915d0214
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2024-10-11 21:39:07 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 112 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,31 +1,36 @@
 from collections import deque
 def bfs():
-    q = deque()
-    q.append((x_h,y_h))
-    while q:
-        x, y = q.popleft()
-        if abs(x-x_fes) + abs(y-y_fes) <= 1000:
+    queue = deque()
+    queue.append((home_x,home_y))
+    while queue:
+        x, y = queue.popleft()
+        if abs(x-festa_x) + abs(y-festa_y) <= 1000: # 맥주를 리필할 필요가 있는가? No!
             print('happy')
-            return
-        for i in range(n):
-            if not visited[i]:
-                nx, ny = graph[i]
-                if abs(x-nx) + abs(y-ny) <= 1000:
-                    q.append((nx,ny))
-                    visited[i] = 1
+            return 0
+        else:
+            for i in range(N):  # 편의점 개수만큼 반복
+                if not visited[i]: # i번째 편의점을 방문 안했다면?
+                    nx, ny = conv[i] # 해당 위치 편의점 좌표 뽑기
+                    if abs(x-nx)+abs(y-ny) <= 1000:
+                        queue.append((nx,ny)) # nx,ny 좌표 위치로 이동
+                        visited[i] = 1  # 방문 체크
     print('sad')
-    return
+    return 0
+            
 
-
-
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    x_h, y_h = map(int,input().split())
-    graph = []
-    for _ in range(n):
-        x, y = map(int, input().split())
-        graph.append((x,y))
-    x_fes, y_fes = map(int, input().split())
-    visited = [0 for _ in range(n+1)]
+    return 0
+T= int(input())
+for _ in range(T):
+    N = int(input())
+    conv = []
+    home_x, home_y = map(int,input().split()) # 상근이네 집 좌표
+    for i in range(N):  # 편의점들 좌표
+        a,b = map(int,input().split())
+        conv.append((a,b))
+    festa_x, festa_y = map(int,input().split()) # 락페 좌표
+    visited = [0 for _ in range(N+1)]
     bfs()
+
+
+
+
