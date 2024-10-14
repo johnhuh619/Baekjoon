dceb6ea23be44be1e3c54deb2c36dd13059d2ede
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2024-10-14 15:10:58 +0900

    [Gold IV] Title: 빙산, Time: 596 ms, Memory: 215924 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,60 +1,54 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+    
+def bfs(i,j):
+    queue = deque()
+    queue.append((i,j))
+    visited[i][j] = 1
 
-def bfs(x, y):
-    q = deque([(x, y)])
-    visited[x][y] = 1
-    seaList = []
-
-    while q:
-        x, y = q.popleft()
-        sea = 0
+    while queue:
+        x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if not graph[nx][ny]:
-                    sea += 1
-                elif graph[nx][ny] and not visited[nx][ny]:
-                    q.append((nx, ny))
-                    visited[nx][ny] = 1
-        if sea > 0:
-            seaList.append((x, y, sea))
-    for x, y, sea in seaList:
-        graph[x][y] = max(0, graph[x][y] - sea)
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+                # 빙산이라면 큐에 추가
+                if sea_origin[nx][ny] != 0:
+                    visited[nx][ny] = True
+                    queue.append([nx, ny])
 
-    return 1
+                # 빙산이 아니라면 바닷물로 카운트
+                else:
+                    cnt[x][y] += 1
+    return 1    
 
-
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-
-ice = []
-for i in range(n):
-    for j in range(m):
-        if graph[i][j]:
-            ice.append((i, j))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+N, M = map(int, input().split())
+sea_origin = []                         # 초기 지도값 저장용
+for _ in range(N):
+    sea_origin.append(list(map(int,input().split())))
 year = 0
 
-while ice:
-    visited = [[0] * m for _ in range(n)]
-    delList = []
-    group = 0
-    for i, j in ice:
-        if graph[i][j] and not visited[i][j]:
-            group += bfs(i, j)
-        if graph[i][j] == 0:
-            delList.append((i, j))
-    if group > 1:
-        print(year)
+while True:
+    answer = []
+    visited = [[0]*M for _ in range(N)]  # 방문 체크용
+    cnt = [[0]*M for _ in range(N)]         # melt/year 기록용
+
+    for i in range(N):
+        for j in range(M):
+            if sea_origin[i][j] != 0 and not visited[i][j]:
+                answer.append(bfs(i,j))
+
+    # 빙산 녹이기
+    for i in range(N):
+        for j in range(M):
+            sea_origin[i][j] -= cnt[i][j]
+            if sea_origin[i][j] < 0:
+                sea_origin[i][j] = 0
+
+    if len(answer) == 0 or len(answer) >= 2:
         break
-    ice = sorted(list(set(ice) - set(delList)))
     year += 1
 
-if group < 2:
-    print(0)
+print(year) if len(answer) >= 2 else print(0)
