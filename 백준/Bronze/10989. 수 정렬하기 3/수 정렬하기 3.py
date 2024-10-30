f6ce220b0c55bd37cc5e7afb30f807111b81f1c4
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2024-10-30 22:01:40 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 4144 ms, Memory: 128160 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,9 @@
+n = int(input())
+numbers = [0]*10001
+for _ in range(n):
+    num = int(input())
+    numbers[num] += 1
+for i in range(1, 10001):
+    if numbers[i] != 0:
+        for _ in range(numbers[i]):
+            print(i)
