80e7e336c0547f287be0991ccd52223662fcb3c4
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2024-11-13 20:18:15 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,22 @@
 from collections import deque
-
 n = int(input())
 m = int(input())
+graph = [[] for _ in range(n+1)]
 
-graph = [[] for i in range(n+1)]
-visited=[0]*(n+1)
 
 for i in range(m):
-    a ,b = map(int,input().split())
-    graph[a]+=[b]
-    graph[b]+=[a]
+    u,v  = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-visited[1]=1
-q=deque([1])
+visited = [0]*(n+1)
+q = deque([1])
+visited[1] = 1
 while q:
-    c = q.popleft()
-    for k in graph[c]:
-        if visited[k] == 0:
-            q.append(k)
-            visited[k] = 1  
+    node = q.popleft()
+    for n_node in graph[node]:
+        if visited[n_node] == 0:
+            q.append(n_node)
+            visited[n_node] = 1
 
 print(sum(visited)-1)
