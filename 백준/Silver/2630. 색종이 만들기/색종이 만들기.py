f9bc75ac8095d72b1f1d4e953109a81c0e0ee9ca
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2024-11-16 21:43:08 +0900

    [Silver II] Title: 색종이 만들기, Time: 112 ms, Memory: 111608 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,23 +1,26 @@
 import sys
-
-def div(y, x, n):
+def div(y,x,n):
+    # 초기 색 좌표 넘겨주는듯?
     color = paper[y][x]
     for i in range(y, y+n):
         for j in range(x, x+n):
             if color != paper[i][j]:
-                m=n//2
-                div(y,x,m)
-                div(y,x+m,m)
-                div(y+m,x,m)
-                div(y+m,x+m,m)
+                new_n = n//2
+                div(y,x,new_n)              # 1            
+                div(y,x+new_n,new_n)        # 2
+                div(y+new_n,x+new_n,new_n)  # 3
+                div(y+new_n,x,new_n)        # 4
                 return
     if color == 0:
-        result[0] += 1
+        res[0] += 1
     else:
-        result[1] += 1
+        res[1] += 1
+    
+
+
 
 n = int(sys.stdin.readline())
-paper = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-result = [0,0]
+paper = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+res = [0,0]
 div(0,0,n)
-print(result[0], '\n', result[1], sep='')
+print(res[0], '\n', res[1], sep='')
