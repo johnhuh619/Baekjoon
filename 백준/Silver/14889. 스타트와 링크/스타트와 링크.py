bf6d8382d72c68b8360d5aeffd4a83d0a0e2c52a
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2024-11-28 15:06:51 +0900

    [Silver I] Title: 스타트와 링크, Time: 1384 ms, Memory: 114936 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,28 @@
+n = int(input())
+board = []
+for _ in range(n):
+    board.append(list(map(int,input().split())))
+visited = [False for _ in range(n)]
+res = 1e9
+def dfs(man,idx):
+    global res
+    if man == n//2:
+        # min 한지 검사
+        x, y = 0,0 
+        for i in range(n):
+            for j in range(n):
+                if visited[i] and visited[j]:
+                    x += board[i][j]
+                elif not visited[i] and not visited[j]:
+                    y += board[i][j]
+        
+        res = min(res,abs(x-y))
+        return
+    
+    for i in range(idx,n):
+        if not visited[i]:
+            visited[i] = True
+            dfs(man+1,i+1)
+            visited[i] = False
+dfs(0,0)
+print(res)
