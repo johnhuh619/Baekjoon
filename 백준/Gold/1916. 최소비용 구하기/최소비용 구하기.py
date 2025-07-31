5a8f49cba039c4d6b6b50e85817266b703c8595e
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-08-01 00:18:46 +0900

    [Gold V] Title: 최소비용 구하기, Time: 472 ms, Memory: 128072 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,28 +1,27 @@
 import heapq
-
 n = int(input())
 m = int(input())
 graph = [[] for _ in range(n+1)]
 for _ in range(m):
-    s,e,v = map(int,input().split())
-    graph[s].append((e,v))
+  start, end, val = map(int, input().split())
+  graph[start].append((end,val))
 
-start, end = map(int,input().split())
-
+start, end = map(int, input().split())
+# 해당 노드까지의 최단비용 합
 costs = [(1e9) for _ in range(n+1)]
 heap = []
 costs[start] = 0
-heapq.heappush(heap, [0,start])
-
+heapq.heappush(heap,[0,start])
 while heap:
-    cur_cost, cur_v = heapq.heappop(heap)
-    if costs[cur_v] < cur_cost:
-        continue
-    for next_v, next_cost in graph[cur_v]:
-        sum_cost = cur_cost + next_cost
-        if sum_cost >= costs[next_v]:
-            continue
-        costs[next_v] = sum_cost
-        heapq.heappush(heap, [sum_cost, next_v])
+  c_value, c_node = heapq.heappop(heap)
+  if costs[c_node] < c_value:
+    continue
+  for n_node, n_value in graph[c_node]:
+    sum_v = c_value + n_value
+    if sum_v >= costs[n_node]:
+      continue
+    costs[n_node] = sum_v
+    heapq.heappush(heap,[sum_v, n_node])
 
 print(costs[end])
+               
