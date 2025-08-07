c88a62e4e200af41bc972abda8bf50b2eb9c7fe2
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-08-07 10:49:38 +0900

    [Gold V] Title: 강의실 배정, Time: 288 ms, Memory: 137012 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,24 +1,20 @@
-import sys
 import heapq
-input = sys.stdin.readline
-N = int(input())
+n = int(input())
 time = []
+for i in range(n):
+  s, t = map(int, input().split())
+  time.append((s,t))
 
-for _ in range(N):
-    time.append(list(map(int,input().split(' '))))
-    
-time.sort(key=lambda x: (x[0], x[1]))
+time.sort(key=lambda x: x[0])
 
-# 가장 빠른 종료 시간을 힙에서 출력한다
-# 힙을 사용하는 이유는 자동 정렬이 되기 때문이다.
-heap = [time[0][1]]
-for i in range(1,N):
-    # 만약 가장 빠른 종료 시간 <= 현재 강의 시작 시간
-    if heap[0] <= time[i][0]:
-        # 강의실을 이어서 사용이 가능하다 => 종료 시간을 힙에서 제거
-        heapq.heappop(heap)
-    # 현재 강의의 종료 시간을 힙에 추가
-    # 현재 진행 중인 강의의 종료 시간을 체크해 줘야 하니까.
-    heapq.heappush(heap, time[i][1])
+pq= []
+max_rooms = 0
 
-print(len(heap))
+for s,e in time:
+  if pq and pq[0] <= s:
+    heapq.heappop(pq)
+  
+  heapq.heappush(pq,e)
+  max_rooms = max(max_rooms, len(pq))
+
+print(max_rooms)
