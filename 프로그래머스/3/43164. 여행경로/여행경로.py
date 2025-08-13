e940acdbb6a0bb4e1192d9ac2ec10f5031cdf19f
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-08-13 11:01:18 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -2,16 +2,20 @@ from collections import deque, defaultdict
 
 def solution(tickets):
     graph = defaultdict(list)
-    for s,e in tickets:
+    for s, e in tickets:
         graph[s].append(e)
+    
     for s in graph:
         graph[s].sort(reverse=True)
+        
     stack = deque(["ICN"])
     route = []
+    
     while stack:
-        current = stack[-1]    # 맨 위
-        if not graph[current]: # 방문 안했으면, route에 추가.
+        cur = stack[-1]
+        if not graph[cur]:
             route.append(stack.pop())
         else:
-            stack.append(graph[current].pop()) # 방문 했으면, pop
+            stack.append(graph[cur].pop())
     return route[::-1]
+            
