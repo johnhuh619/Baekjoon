7032ba4e6c41cca864b3cf22c24238a3213c3014
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-08-14 21:33:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.64 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 56.7 MB, 시간: 9.05 ms
+메모리: 9.29 MB, 시간: 10.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 06일 23:08:15
+2025년 08월 14일 21:33:49
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,39 +1,26 @@
 from collections import deque
-def solution(maps):
-    answer = 0
 
-    # 상하좌우
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+def bfs(start: tuple, map: list):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    r, c = len(map), len(map[0])
+    q = deque([start])
 
-    def bfs(x, y):
-        queue = deque()
-        queue.append((x, y))
+    while q:
+        x,y = q.popleft()
 
-        # queue가 빌 때까지 반복
-        while queue:
-            x, y = queue.popleft()
-
-            # 상하좌우 칸 확인하기
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-
-                # 맵을 벗어나면 무시하기
-                if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]):
-                    continue
-
-                # 벽이면 무시하기
-                if maps[nx][ny] == 0:
-                    continue
-
-                # 처음 지나가는 길이면 거리계산하고 다시 상하좌우 확인하기
-                if maps[nx][ny] == 1:
-                    maps[nx][ny] = maps[x][y] + 1
-                    queue.append((nx, ny))    # 재귀
-
-        # 상대 팀 진영(제일 오른쪽 아래 칸)까지의 거리 반환
-        return maps[len(maps)-1][len(maps[0])-1]
-
-    answer = bfs(0, 0)
-    return -1 if answer == 1 else answer    # 상대 팀 진영에 도착할 수 없을 때 -1
+        # 벽 = 0
+        # 길 = 1
+        # 길에 누적하여 방문 추적
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < r and 0 <= ny < c and map[nx][ny] == 1:
+                map[nx][ny] = map[x][y] + 1
+                q.append((nx, ny))
+                if nx == r-1 and ny == c-1:
+                    return map[nx][ny]
+    return -1
+def solution(maps):  
+    return bfs((0,0), maps)
