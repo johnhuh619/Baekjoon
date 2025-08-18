01bd9d7ae25b19c72c114d703cce7f5558fec92a
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-08-18 18:06:19 +0900

    [Gold V] Title: 토마토, Time: 688 ms, Memory: 220404 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,40 +1,53 @@
 from collections import deque
 
-def tomato(graph):
-    queue = deque()
 
-    # 익은 토마토와 큐 초기화
+    
+
+
+
+
+def sol():
+    # row, col, height
+    m, n, h = map(int, input().split())
+    box = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+    
+    dx = [1,-1,0,0,0,0]
+    dy = [0,0,1,-1,0,0]
+    dz = [0,0,0,0,1,-1]
+    
+    #bfs
+    q = deque()
     for i in range(h):
-        for j in range(m):
-            for k in range(n):
-                if graph[i][j][k] == 1:
-                    queue.append((i, j, k))
-                    visited[i][j][k] = True
-
-    # BFS 수행
-    dx, dy, dz = [1, -1, 0, 0, 0, 0], [0, 0, 1, -1, 0, 0], [0, 0, 0, 0, 1, -1]
-    day = 0
-    while queue:
-        for _ in range(len(queue)):
-            x, y, z = queue.popleft()
-            for i in range(6):
-                nx, ny, nz = x + dx[i], y + dy[i], z + dz[i]
-                if 0 <= nx < h and 0 <= ny < m and 0 <= nz < n and not visited[nx][ny][nz] and graph[nx][ny][nz] == 0:
-                    queue.append((nx, ny, nz))
-                    visited[nx][ny][nz] = True
-                    graph[nx][ny][nz] = 1
-        day += 1
-
-    # 모든 토마토가 익었는지 확인
-    for i in range(h):
-        for j in range(m):
-            for k in range(n):
-                if graph[i][j][k] == 0:
+        for j in range(n):
+            for k in range(m):
+                if box[i][j][k] == 1:
+                    q.append((i,j,k))
+                    
+    while q:
+        z, y, x = q.popleft()
+        
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
+            
+            if 0 <= nx < m and 0 <= ny < n and 0<= nz < h and box[nz][ny][nx] == 0:
+                box[nz][ny][nx] = box[z][y][x] + 1
+                q.append((nz,ny,nx))
+    
+    ans = 0
+    for z in range(h):
+        for y in range(n):
+            for x in range(m):
+                t = box[z][y][x]                                    
+                if  t == 0:
                     return -1
-    return day - 1 if day > 0 else 0
+                    
+                if t == -1:
+                    continue
+                ans = max(ans, t)
+    return (ans - 1)
 
-# 입력 받기
-n, m, h = map(int, input().split())
-graph = [[list(map(int, input().split())) for _ in range(m)] for _ in range(h)]
-visited = [[[False] * n for _ in range(m)] for _ in range(h)]
-print(tomato(graph))
+
+print(sol())
+    
