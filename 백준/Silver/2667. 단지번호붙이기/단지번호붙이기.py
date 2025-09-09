2b79283e37ba608c33874076a63f5d5e9cf80efa
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-09-09 23:36:32 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 113496 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,37 @@
 from collections import deque
-import sys
+
 n = int(input())
-graph = []
-ans = []
-for _ in range(n):
-    graph.append(list(map(int,sys.stdin.readline().rstrip())))
+graph= [list(map(int,input().strip())) for _ in range(n)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 def bfs(x,y):
-    q = deque()
-    q.append((x,y))
-
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
-    cnt = 1
+    q = deque([(x,y)])
     graph[x][y] += 1
-    while q:
-        x,y = q.popleft()
-        
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
-                q.append((nx,ny))
-                cnt += 1
-                graph[nx][ny] += 1  
-    ans.append(cnt)
+    size = 1
 
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
+                    size += 1
+                    graph[nx][ny] += 1
+                    q.append((nx, ny))
+
+    return size    
+
+arr = []
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
-            bfs(i,j)
-ans.sort()
-print(len(ans))
-for i in ans:
+            arr.append(bfs(i,j))
+
+arr.sort()
+print(len(arr))
+for i in arr:
     print(i)
+
+
