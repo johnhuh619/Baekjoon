db6e7e5fd646f13b18a67da6021f5953f1100730
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-09-09 22:30:39 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 436 ms, Memory: 175128 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -6,13 +6,16 @@ for i in range(m):
     graph[v].append(u)
 
 visited = [False]*(n+1)
-
-def dfs(n):
-    visited[n] = True
-    for node in graph[n]:
-        if not visited[node]:
-            visited[node] = True
-            dfs(node)
+stack = []
+def dfs(start):
+    stack.append(start) 
+    visited[start] = True
+    while stack:   
+        cur = stack.pop()
+        for nxt in graph[cur]:
+            if not visited[nxt]:
+                visited[nxt] = True
+                stack.append(nxt)
 cnt = 0
 for i in range(1, n+1):
     if not visited[i]:
