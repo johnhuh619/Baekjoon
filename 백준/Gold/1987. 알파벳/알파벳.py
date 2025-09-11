fbf01e18ce8d2180f7b3d1b8787b3ca6e22beae1
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-09-11 23:18:41 +0900

    [Gold IV] Title: 알파벳, Time: 4652 ms, Memory: 113528 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,21 +1,23 @@
-R, C = map(int,input().split())
-board = [list(input().strip()) for _ in range(R)]
-visited = set(board[0][0])
-dir = [(0,1),(1,0),(-1,0),(0,-1)]
-res = 0
-def dfs(x,y,cnt):
-    
-    global res
-    res = max(res,cnt)
-    
-    for dx,dy in dir:
-        nx, ny = x + dx, y + dy
-        if 0<=nx<R and 0<=ny<C:
-            if board[nx][ny] not in visited:
-                visited.add(board[nx][ny])
-                dfs(nx,ny,cnt+1)
-                visited.remove(board[nx][ny])
+from collections import deque
+r, c = map(int, input().split())
+graph = [list(input().rstrip()) for _ in range(r)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-    
-dfs(0,0,1)
-print(res)
+
+def dfs():
+    mcnt = 0
+    stack = deque([(0,0,graph[0][0])])    
+
+    while stack:
+        x, y, route = stack.pop()
+        mcnt = max(mcnt, len(route))
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < r and 0 <= ny < c:
+                if graph[nx][ny] not in route:
+                    stack.append((nx, ny, route + graph[nx][ny]))
+    return mcnt
+print(dfs())             
