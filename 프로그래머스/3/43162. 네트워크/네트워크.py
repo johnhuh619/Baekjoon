ade8ff63a4219bffb23e110b5afa8c01bb2c69ad
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-09-11 22:38:25 +0900

    [level 3] Title: 네트워크, Time: 0.54 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,22 @@
+from collections import deque
+# n = node 개수
+# computers = 노드 모음집
 def solution(n, computers):
     
     visited = [False] * n
+    def dfs(start):
+        stack = deque([start])
+        while stack:
+            cur = stack.pop()            
+            for i in range(len(computers[cur])):
+                if computers[cur][i] == 1 and not visited[i]:
+                    stack.append(i)
+                    visited[i] = True
+    
     cnt = 0
-
     for i in range(n):
         if not visited[i]:
-            stack = [i]
-
-            while stack:
-                node = stack.pop()
-                if not visited[node]:
-                    visited[node] = True
-                    for j in range(n):
-                        if computers[node][j] == 1 and not visited[j]:
-                            stack.append(j)
-            
+            dfs(i)
             cnt += 1
+    
     return cnt
