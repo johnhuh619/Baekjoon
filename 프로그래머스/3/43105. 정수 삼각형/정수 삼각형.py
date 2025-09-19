704944ae32d4f5603f1f3213f7f358fcd69ce9a5
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-09-19 21:04:55 +0900

    [level 3] Title: 정수 삼각형, Time: 36.88 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,8 +1,10 @@
 def solution(triangle):
-    floor = len(triangle)-1
-    while floor > 0:
-        for i in range(floor):
-            triangle[floor-1][i] += max(triangle[floor][i], triangle[floor][i+1])
-        floor-=1
-    
+    depth = len(triangle) -1 
+    while depth >0:
+        for i in range(depth):
+            triangle[depth-1][i] += max(triangle[depth][i], triangle[depth][i+1])
+        depth -= 1
     return triangle[0][0]
+
+# 이동 규칙
+# cur_idx 기준 => cur_idx & cur_idx + 1
