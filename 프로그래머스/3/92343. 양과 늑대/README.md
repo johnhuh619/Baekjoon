5febf60b3457eb2860f941878af27be0d10195e7
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-10-28 23:01:27 +0900

    [level 3] Title: 양과 늑대, Time: 9.62 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 10.16 ms
+메모리: 9.21 MB, 시간: 9.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 24일 23:59:16
+2025년 10월 28일 23:01:25
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,26 +1,33 @@
 from collections import deque
 def solution(info, edges):
+    # wolf, sheep
+    # 전공
+    # lv.2
     ans = 0
     graph = [[] for _ in range(len(info))]
-    for e in edges:
-        graph[e[0]].append(e[1])
-        
+    
+    for s, e in edges:
+        graph[s].append(e)
+    
+    # s, w, visited
     stack = deque([(1, 0, set(graph[0]))])
-        
+    max_sheep = 0
     while stack:
-        cs, cw, cc = stack.pop()
-        ans = max(ans, cs)
+        cs, cw, can = stack.pop()
+        max_sheep = max(max_sheep, cs)
         
-        for nxt in cc:
-            nc = cc.copy()
-            nc.remove(nxt)
-            nc.update(graph[nxt])
-        
-            if info[nxt] == 0:
-                stack.append((cs+1, cw, nc))
+        # [[2,3],[4,5],[6,7]]
+        # [2,3]
+        # [3,4,5] / [2,6,7]
+        for nxt in can:
+            n_can = can.copy()
+            n_can.remove(nxt)
+            n_can.update(graph[nxt])
             
+            if info[nxt] == 0:
+                stack.append((cs+1, cw, n_can))
             else:
                 if cs > cw+1:
-                    stack.append((cs, cw+1, nc))
+                    stack.append((cs, cw+1, n_can))
             
-    return ans
+    return max_sheep
