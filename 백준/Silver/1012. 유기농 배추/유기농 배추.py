11433ba7877099ebbbd947669b37ade17186762f
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-11-10 23:03:43 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 114308 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,41 +1,48 @@
+# 입력의 첫 줄에는 테스트 케이스의 개수 T가 주어진다. 
+# 그 다음 줄부터 각각의 테스트 케이스에 대해 
+# 첫째 줄에는 배추를 심은 배추밭의 가로길이 M(1 ≤ M ≤ 50)과 세로길이 N(1 ≤ N ≤ 50), 
+# 그리고 배추가 심어져 있는 위치의 개수 K(1 ≤ K ≤ 2500)이 주어진다. 
+# 그 다음 K줄에는 배추의 위치 X(0 ≤ X ≤ M-1), Y(0 ≤ Y ≤ N-1)가 주어진다. 
+# 두 배추의 위치가 같은 경우는 없다.
+
 from collections import deque
-import sys
-i = int(input())
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-def bfs(x,y,graph):
-    q = deque()
-    q.append((x,y))
-    graph[x][y] = 0
-
-    while q:
-        x,y = q.popleft()
-        
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                q.append((nx,ny))
-    return
-
-
-
-for i in range(i):
-    n, m, s = map(int,input().split())
-    graph = [[0]*(m) for _ in range(n)]
+def sol():
+    M, N, K = map(int, input().split())
+    field = [[0]*M for _ in range(N)]
+    for i in range(K):
+        x, y = map(int, input().split())
+        field[y][x] = 1
+    visited = [[False]*M for _ in range(N)]
     cnt = 0
 
-    for _ in range(s):
-        x,y = map(int,input().split())
-        graph[x][y] = 1
+    def bfs(i,j):
+        dx= [1,-1,0,0]
+        dy = [0,0,1,-1]
+        q = deque([(i,j)])
 
-    for x in range(n):
-        for y in range(m):
-            if graph[x][y] == 1:
-                bfs(x,y,graph)
-                cnt+=1
-    print(cnt)
+        while q:
+            y, x = q.popleft()
+            for n in range(4):
+                nx = x + dx[n]
+                ny = y + dy[n]
+                if 0 > nx or nx >= M or 0 > ny or ny >= N:
+                    continue
+                if not visited[ny][nx] and field[ny][nx] == 1:
+                    visited[ny][nx] = True
+                    q.append((ny, nx))
+        
+        
+    for y in range(N):
+        for x in range(M):
+            if field[y][x]  == 1 and not visited[y][x]:
+                bfs(y,x)
+                cnt +=1
+    return cnt        
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        print(sol())
+
+main()
