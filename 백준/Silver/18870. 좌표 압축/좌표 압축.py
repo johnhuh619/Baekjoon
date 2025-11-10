82ccb53819e8c07d4862b69f905a1162240d45d1
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-11-11 00:03:03 +0900

    [Silver II] Title: 좌표 압축, Time: 752 ms, Memory: 287408 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,10 +1,13 @@
-import sys
-n = int(input())
-num = list(map(int,sys.stdin.readline().split()))
-s = sorted(set(num))
-dic = {s[i]: i for i in range(len(s))}
-res = []
-for v in num:
-    res.append(dic[v])
-print(*res)
+# 첫째 줄에 N이 주어진다.
+# 둘째 줄에는 공백 한 칸으로 구분된 X1, X2, ..., XN이 주어진다.
 
+def sol():
+    N = int(input())
+    ns = list(map(int, input().split()))
+    sorted_set = sorted(set(ns))
+    rank = {s: idx for idx, s in enumerate(sorted_set)}
+    ans = [rank[n] for n in ns]
+    print(' '.join(map(str, ans)))
+
+sol()
+    
