9f72b9a0903bf4da0612ec4450c3be87aa876530
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-11-10 23:32:32 +0900

    [Silver II] Title: 랜선 자르기, Time: 112 ms, Memory: 110944 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,19 +1,29 @@
-k, n = map(int, input().split())
-wires = [int(input()) for x in range(k)]
+# 첫째 줄에는 오영식이 이미 가지고 있는 랜선의 개수 K, 그리고 필요한 랜선의 개수 N이 입력된다.
+# K는 1이상 10,000이하의 정수이고, N은 1이상 1,000,000이하의 정수이다.
+# 그리고 항상 K ≦ N 이다.
+# 그 후 K줄에 걸쳐 이미 가지고 있는 각 랜선의 길이가 센티미터 단위의 정수로 입력된다.
+# 랜선의 길이는 231-1보다 작거나 같은 자연수이다.
 
-def cut_wire(cut):
-    tot = 0
-    for wire_len in wires:
-        tot += wire_len // cut
-    return n <= tot
-
-l = 1
-r = max(wires) + 1
-while l < r:
-    mid = (l+r) //2
-    if cut_wire(mid):
-        l = mid +1
-    else:
-        r = mid
+def sol():
+    K, N = map(int, input().split())
+    ns = [int(input()) for _ in range(K)]
+    high = max(ns)
+    low = 1
+    ans = 0
+    def check(mid):
+        cnt = 0
+        for n in ns:
+            cnt += n // mid
+        return cnt
         
-print(l-1)
+    while low <= high:
+        mid = (low + high) // 2
+        cur_k = check(mid)
+        if  cur_k >= N:
+            ans = mid
+            low = mid+1
+        else:
+            high = mid-1
+    return ans
+
+print(sol())
