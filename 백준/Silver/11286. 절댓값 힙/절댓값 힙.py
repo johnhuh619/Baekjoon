68acca4a4471faeb1e8a75c61720e27db76b9591
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-12-15 18:09:49 +0900

    [Silver I] Title: 절댓값 힙, Time: 184 ms, Memory: 116412 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,14 +1,16 @@
-import sys, heapq
-# 튜플을 이용하면, 앞에거 먼저 비교함. if 앞에가 같다? -> 뒤에거 비교
-# 따라서 절댓값 값을 먼저 넣고, 그다음 넘을 넣음.
-abs_heap = []
-n = int(sys.stdin.readline())
+import heapq, sys
+n = int(input())
+q = []
+
+heapq.heapify(q)
+
 for i in range(n):
     num = int(sys.stdin.readline())
     if num:
-        heapq.heappush(abs_heap, (abs(num),num))
+        heapq.heappush(q, (abs(num), num))
+    
     else:
-        if abs_heap:
-            print(heapq.heappop(abs_heap)[1])
+        if q:
+            print(heapq.heappop(q)[1])
         else:
-            print(0)
+            print(0)    
