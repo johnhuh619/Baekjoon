0d1b7aa75b53a60a707b38d7c9d305f00c51dd98
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-12-16 01:53:35 +0900

    [Silver I] Title: 경로 찾기, Time: 116 ms, Memory: 111048 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,41 +1,14 @@
 import sys
+input = sys.stdin.readline
 
 n = int(input())
-graph = [[] for _ in range(n)]
+graph = [list(map(int,input().split())) for _ in range(n)]
 
-for i in range(n):
-    l = list(map(int, input().split()))
-    for j in range(n):
-        if l[j] == 1:
-            graph[i].append(j)
-res = [[0]*n for _ in range(n)]
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
 
-def dfs(start):
-    visited = [0] * n
-    stack = [start]
-    
-    while stack:
-        cur = stack.pop()
-        for nxt in graph[cur]:
-            if not visited[nxt]:
-                visited[nxt] = 1
-                stack.append(nxt)
-
-    return visited
-
-for i in range(n):
-    visited = dfs(i)
-    for j in range(n):
-        res[i][j] = visited[j]
-
-for row in res:
+for row in graph:
     print(*row)
-
-            
-
-                
-            
-    
-    
-
-        
