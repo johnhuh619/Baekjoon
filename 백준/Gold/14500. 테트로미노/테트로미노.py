a4e131d063d4a124ca785e28e134edcd73a139b0
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2025-12-19 20:19:03 +0900

    [Gold IV] Title: 테트로미노, Time: 796 ms, Memory: 117028 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,43 +1,47 @@
-import sys
-from collections import deque
+
 n, m = map(int,input().split())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+graph = [list(map(int,input().split())) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
+
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
-maxVal = 0
-visited = [[False]*m for _ in range(n)]
-def dfs(x,y, depth, current_sum):
-    global maxVal
+
+ans = 0
+def dfs(x, y, depth, tot):
+    global ans
+    
     if depth == 4:
-        maxVal = max(maxVal, current_sum)
+        ans = max(ans, tot)
         return
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        
-        if 0<=nx<n and 0 <= ny < m and not visited[nx][ny]:
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
             visited[nx][ny] = True
-            dfs(nx, ny, depth+ 1, current_sum + graph[nx][ny])
+            dfs(nx, ny, depth + 1, tot + graph[nx][ny])
             visited[nx][ny] = False
-            
-def check_exception(x,y):
-    global maxVal
+
+def check_ex(x, y):
+    global ans
+    vals = []
+    center = graph[x][y]
     for i in range(4):
-        temp = graph[x][y]
-        for j in range(3):
-            k = (i+j) % 4
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if 0 <= nx < n and 0<= ny < m:
-                temp += graph[nx][ny]
-            else:
-                break
-        maxVal = max(maxVal,temp)
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m:
+            vals.append(graph[nx][ny])
+    
+    if len(vals) >= 3:
+        ans = max(ans, center + sum(vals) - (min(vals) if len(vals) == 4 else 0))
 
 for i in range(n):
     for j in range(m):
+        # 연속된 모양 처리
         visited[i][j] = True
         dfs(i,j,1,graph[i][j])
         visited[i][j] = False
-        check_exception(i,j)
-print(maxVal)
+
+        # T 자 처리
+        check_ex(i,j)
+
+print(ans)
