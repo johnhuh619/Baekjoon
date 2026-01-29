fc341fcce10a6a0c3e1787ff3eb0a21b928f16ac
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-01-29 19:24:37 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 116 ms, Memory: 123248 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+s = input().rstrip()
+
+ans = []
+stack = []
+in_tag = False
+for ch in s:
+    if ch == '<':
+        while stack:
+            ans.append(stack.pop())
+        in_tag = True
+        ans.append(ch) 
+    elif ch == '>':
+        in_tag = False
+        ans.append(ch)
+    elif in_tag:
+        ans.append(ch)    
+    else:
+        if ch == ' ':
+            while stack:
+                ans.append(stack.pop())
+            ans.append(' ')
+        else:
+            stack.append(ch)
+while stack:
+    ans.append(stack.pop())
+    
+print(''.join(ans))
