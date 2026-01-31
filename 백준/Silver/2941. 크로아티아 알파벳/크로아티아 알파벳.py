b0458547a61e9b7fa7ffc1561706b91e6f430959
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-02-01 01:56:15 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -2,7 +2,6 @@ arr = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=' ]
 word = input()
 
 for w in arr:
-    while word.find(w) > -1:
-        word = word.replace(w, ' ')
+    word = word.replace(w, ' ')
 ans = len(word)
 print(ans)
