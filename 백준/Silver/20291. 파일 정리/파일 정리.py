b9ab77c46dc4bfda8079bd10c26b6ea6455ae7c7
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-02-02 21:09:23 +0900

    [Silver III] Title: 파일 정리, Time: 240 ms, Memory: 125820 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,10 +1,8 @@
 n = int(input())
 dic = {}
 for _ in range(n):
-    file = input().rstrip()
-    fname, ftype = file.split('.')
-    dic[ftype] = dic.get(ftype, 0) + 1
+    fname, typ = map(str, input().split('.'))
+    dic[typ] = dic.get(typ, 0) + 1
 
 for f in sorted(dic):
     print(f, dic[f])
-    
