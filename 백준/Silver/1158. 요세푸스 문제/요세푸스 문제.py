4cb17d930b596d6b7eb6ad1ec4fc80195fbb3d1d
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-02-04 15:53:10 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 448 ms, Memory: 244524 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,13 +1,11 @@
 from collections import deque
-n, k = map(int ,input().split())
+n, k = map(int,input().split())
 
-def y_prob(n, k):
-    q = deque([i for i in range(1,n+1)])
-    ans = []
-    while q:
-        for _ in range(k-1):
-            q.append(q.popleft())
-        ans.append(q.popleft())        
-    return ans
-
-print("<" +', '.join(map(str, y_prob(n,k)))+">" )
+q = deque([i for i in range(1,n+1)])
+ans = []
+while q:
+    for _ in range(k-1):
+        q.append(q.popleft())
+    ans.append(str(q.popleft()))        
+    
+print(f"<{', '.join(ans)}>")
