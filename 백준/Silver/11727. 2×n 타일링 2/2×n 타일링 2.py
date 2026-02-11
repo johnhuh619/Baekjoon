21d0be80aea18e60c062b2883084540f466b0ae0
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-02-11 23:47:08 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,9 +1,20 @@
 n = int(input())
-dp = [0] * 1001
-dp[1] = 1
-if n >= 2:
-    dp[2] = 3
-    for i in range(3,n+1):
-        dp[i] = dp[i-2]*2 + dp[i-1]
+# 1*2 / 2*1 / 2*2
+# 2 / 1 / 1
+# n1 = a
+# n2 = c / 2b / 2a
+# n3 = (3a) / (c) + (a) / (2b) + (a) / a + c / a + 2b
+# n4 = ()
 
-print(dp[n]%10007)
+# dp[n] = dp[n-1] + dp[n-2] * 2     
+
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(3)
+else:
+    b, a = 3, 1
+    for _ in range(3,n+1):
+        b, a = (b + a*2)%10007 , b
+    print(b)
