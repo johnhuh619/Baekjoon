733deb2f2e86f80394ec45bf499dd4d1ce1760e0
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-03-21 21:59:46 +0900

    [Gold IV] Title: 알고스팟, Time: 144 ms, Memory: 112064 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,37 +1,31 @@
-from collections import deque
-import sys
-
-input = sys.stdin.readline
-
-m, n = map(int, input().split())  # 가로, 세로
+import heapq
+m, n = map(int, input().split())
 board = [list(map(int, input().strip())) for _ in range(n)]
 
 INF = int(1e9)
 dist = [[INF] * m for _ in range(n)]
 dist[0][0] = 0
 
-dq = deque()
-dq.append((0, 0))
+pq = []
+heapq.heappush(pq,(0,0,0))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
-while dq:
-    x, y = dq.popleft()
+while pq:
+    w, x, y = heapq.heappop(pq)
 
+    if dist[x][y] < w:
+        continue
+    
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-
         if 0 <= nx < n and 0 <= ny < m:
-            cost = dist[x][y] + board[nx][ny]
+            nw = w + board[nx][ny]
 
-            if cost < dist[nx][ny]:
-                dist[nx][ny] = cost
+            if nw < dist[nx][ny]:
+                dist[nx][ny] = nw
+                heapq.heappush(pq, (nw, nx, ny))
 
-                if board[nx][ny] == 0:
-                    dq.appendleft((nx, ny))
-                else:
-                    dq.append((nx, ny))
-
-print(dist[n - 1][m - 1])
+print(dist[n-1][m-1])
