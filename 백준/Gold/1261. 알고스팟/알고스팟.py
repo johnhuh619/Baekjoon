8393e497bb611a30f75fc99d2c32dae843f54664
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-03-24 15:21:53 +0900

    [Gold IV] Title: 알고스팟, Time: 152 ms, Memory: 112068 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,31 +1,29 @@
 import heapq
 m, n = map(int, input().split())
-board = [list(map(int, input().strip())) for _ in range(n)]
+graph = [list(map(int, input().strip())) for _ in range(n)] 
 
 INF = int(1e9)
-dist = [[INF] * m for _ in range(n)]
-dist[0][0] = 0
+pq = [(0,0,0)]
+cost = [[INF]*m for _ in range(n)]
+cost[0][0] = 0
 
-pq = []
-heapq.heappush(pq,(0,0,0))
-
-dx = [1,-1,0,0]
+dx = [1, -1, 0, 0]
 dy = [0,0,1,-1]
 
 while pq:
-    w, x, y = heapq.heappop(pq)
-
-    if dist[x][y] < w:
+    c, x, y = heapq.heappop(pq)
+    
+    if c > cost[x][y]:
         continue
     
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
+
         if 0 <= nx < n and 0 <= ny < m:
-            nw = w + board[nx][ny]
+            nc = graph[nx][ny] + c
+            if nc < cost[nx][ny]:
+                cost[nx][ny] = nc
+                heapq.heappush(pq,(nc, nx, ny))
 
-            if nw < dist[nx][ny]:
-                dist[nx][ny] = nw
-                heapq.heappush(pq, (nw, nx, ny))
-
-print(dist[n-1][m-1])
+print(cost[n-1][m-1])
