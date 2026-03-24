1e77aa35207359626cee0f20fae8c723bd329132
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-03-24 14:36:59 +0900

    [Gold II] Title: 트리의 지름, Time: 312 ms, Memory: 146532 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,38 +1,43 @@
 v = int(input())
-graph = [[] for _ in range(v + 1)]
+
+graph = [[] for _ in range(v+1)]
 for _ in range(v):
-    data = list(map(int, input().split()))
-    node = data[0]
-    idx = 1
-    while data[idx] != -1:
-        nxt = data[idx]
-        cost = data[idx + 1]
-        graph[node].append((nxt, cost))
-        idx += 2
+    data = list(map(int , input().split()))
+    cur = data[0]
+    i = 1
+    while data[i] != -1:
+        node, cost = data[i], data[i+1]
+        graph[cur].append((node, cost))
+        i += 2
     
 
+
 def dfs(start):
+    stack = [start]
     visited = [-1] * (v+1)
     visited[start] = 0
-    stack = [start]
     
     while stack:
-        n = stack.pop()
-        for nxt, cost in graph[n]:
+        cur = stack.pop()
+
+        for nxt, cost in graph[cur]:
             if visited[nxt] == -1:
-                visited[nxt] = visited[n] + cost
+                new_cost = cost + visited[cur]
+                visited[nxt] = new_cost
                 stack.append(nxt)
     
     max_node = 1
     max_dist = 0
+    
     for i in range(1, v+1):
-        if visited[i] > max_dist:
+        if max_dist < visited[i]:
             max_dist = visited[i]
             max_node = i
-
+            
     return max_node, max_dist
 
-fn, dist = dfs(1)
-mn, ans = dfs(fn)
+far_node, _ = dfs(1)
+_, ans = dfs(far_node)
 
 print(ans)
+
