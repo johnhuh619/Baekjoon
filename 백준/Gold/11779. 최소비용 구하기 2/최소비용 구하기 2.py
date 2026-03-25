3211393f958626dfc781ed08af71c22d18a73db4
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-03-25 22:57:47 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 276 ms, Memory: 122460 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,42 @@
+import heapq
+
+n = int(input())
+m = int(input())
+
+nodes = [[] for _ in range(n+1)]
+for _ in range(m):
+    s, e, cost = map(int, input().split())
+    nodes[s].append((e, cost))
+
+start, end = map(int, input().split())
+
+INF = int(1e9)
+dist = [INF] * (n+1)
+prev = [[] for _ in range(n+1)]
+def djikstra(s):
+    pq = [(0, s)]
+    heapq.heapify(pq)
+    dist[s] = 0
+    while pq:
+        cost, node = heapq.heappop(pq)
+        
+        if dist[node] < cost:
+            continue
+            
+        for nxt, nc in nodes[node]:
+            new_cost = nc + dist[node]
+            if dist[nxt] > new_cost:
+                dist[nxt] = new_cost
+                prev[nxt] = node
+                heapq.heappush(pq, (new_cost, nxt))
+
+djikstra(start)
+path = []
+cur = end
+while cur:
+    path.append(cur)
+    cur = prev[cur]
+path = path[::-1]
+print(dist[end])
+print(len(path))
+print(*path)
