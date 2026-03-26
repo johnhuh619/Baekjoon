f2f924f14eec05a264bd726f40a119ed550ce0b1
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-03-26 22:07:21 +0900

    [Gold III] Title: 파티, Time: 176 ms, Memory: 112000 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,45 +1,36 @@
 import heapq
 
+n, m, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+rev_graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, w = map(int, input().split())
+    graph[a].append((b,w))
+    rev_graph[b].append((a,w))
+
+
 INF = int(1e9)
+def djikstra(start, graph):
+    dist = [INF] * (n+1)
+    dist[start] = 0
+    pq = [(0, start)]
+    while pq:
+        cost, cur = heapq.heappop(pq)
+        if dist[cur] < cost:
+            continue
+        
+        for nxt, nc in graph[cur]:
+            new_cost = nc + cost
+            if dist[nxt] > new_cost:
+                dist[nxt] = new_cost
+                heapq.heappush(pq, (new_cost, nxt))
+    return dist
 
+go = djikstra(x, graph)
+back = djikstra(x, rev_graph)
 
+max_dist = 0
+for i in range(1, n+1):
+    max_dist = max(go[i] + back[i], max_dist)
 
-def sol():
-    N, M, X = map(int, input().split())
-    graph = [[] for _ in range(N+1)]
-    for _ in range(M):
-        start, end, val = map(int,input().split())
-        graph[start].append((end, val))
-    
-    
-    def djikstra(start):
-        q = []
-        dist = [INF]*(N+1)
-        dist[start] = 0
-            
-        heapq.heappush(q, (0, start))
-            
-        while q:
-            val, node = heapq.heappop(q)
-            
-            if dist[node] < val:
-                continue
-            
-            for nNode_idx, nNode_val in graph[node]:
-                nxt_val = nNode_val + val
-                if dist[nNode_idx] > nxt_val:
-                    dist[nNode_idx] = nxt_val
-                    heapq.heappush(q, (nxt_val, nNode_idx))
-                    
-        return dist
-    
-    res = 0
-    from_x = djikstra(X)
-    
-    for i in range(1, N+1):
-        to_x = djikstra(i)
-        res = max(res, (to_x[X] + from_x[i]))
-    
-    return res
-
-print(sol())
+print(max_dist)
