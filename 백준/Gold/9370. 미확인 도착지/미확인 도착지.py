e3bee1aeaab4aea5a2faa94cd3416a90f2f4a3dd
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-03-30 22:22:08 +0900

    [Gold II] Title: 미확인 도착지, Time: 336 ms, Memory: 121464 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -3,58 +3,51 @@ import heapq
 T = int(input())
 INF = int(1e9)
 def djikstra(start, edges, n):
-    pq = [(0,start)]
+    pq = [(0, start)]
     dist = [INF] * (n+1)
     dist[start] = 0
-    
     while pq:
-        cost, cur = heapq.heappop(pq)
-        if dist[cur] < cost:
+        cost, node = heapq.heappop(pq)
+
+        if cost > dist[node]:
             continue
         
-        for nxt, nc in edges[cur]:
-            new_cost = nc + dist[cur]
-            if dist[nxt] > new_cost:
-                dist[nxt] = new_cost
-                heapq.heappush(pq, (new_cost, nxt))
+        for new_cost, nxt_node in edges[node]:
+            nc = new_cost + dist[node]
+            if nc < dist[nxt_node]:
+                dist[nxt_node] = nc
+                heapq.heappush(pq, (nc, nxt_node))
+    
     return dist
-    
-    
-for _ in range(T):
-    # 교차로/ 도로/ 목적지
-    n, m, t = map(int, input().split())
 
-    # s = start / g <-> h = 지나간 edge 1개
+for _ in range(T):
+    n, m, t = map(int, input().split())
     s, g, h = map(int, input().split())
-    
+
     edges = [[] for _ in range(n+1)]
     for _ in range(m):
-        # a <-> b = d
         a, b, d = map(int, input().split())
-        edges[a].append((b, d))
-        edges[b].append((a, d))
+        edges[b].append((d,a))
+        edges[a].append((d,b))
         if (a == g and b == h) or (a == h and b == g):
-            gh_dist = d
+            dist_gh = d
         
-    end = []
+    ends = []
     for _ in range(t):
         x = int(input())
-        end.append(x)
-        
-    end.sort()
+        ends.append(x)
+    ends.sort()
 
     dist_start = djikstra(s, edges, n)
     dist_g = djikstra(g, edges, n)
     dist_h = djikstra(h, edges, n)
     
     ans = []
-    
-    for x in end:
-        p1 = dist_start[g] + gh_dist + dist_h[x]
-        p2 = dist_start[h] + gh_dist + dist_g[x]
-        
-        if dist_start[x] == p1 or dist_start[x] == p2:
-            ans.append(x)
+    for e in ends:
+        p1 = dist_start[g] + dist_gh + dist_h[e]
+        p2 = dist_start[h] + dist_gh + dist_g[e]
+        if dist_start[e] == p1 or dist_start[e] == p2:
+           ans.append(e) 
     
     print(*ans)
-        
+    
