b30b05f9e05f4ccbcd7cd738b10b0e3f594beb68
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-03-31 23:42:23 +0900

    [Gold III] Title: 불!, Time: 480 ms, Memory: 179896 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,64 +1,56 @@
 from collections import deque
-import sys
+r, c = map(int, input().split())
+board = [list(input().strip()) for _ in range(r)]
+visited_fire = [[0 for _ in range(c)] for _ in range(r)]
+visited_j = [[0 for _ in range(c)] for _ in range(r)]
 
-N, M = map(int, sys.stdin.readline().split())
-maze = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
-visited_fire = [[0 for _ in range(M)] for _ in range(N)]
-visited_J = [[0 for _ in range(M)] for _ in range(N)]
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
-pos_J = None
-pos_fire = None
-
-# Bfs -> queue
-fire_q = deque()
-J_q = deque()
-
-for j in range(N):
-    for i in range(M):
-        if maze[j][i] == 'J':
-            pos_J = (i, j)
-            J_q.append(pos_J)
-            visited_J[j][i] = 1
-        elif maze[j][i] == 'F':
-            pos_fire = (i, j)
-            fire_q.append(pos_fire)
-            visited_fire[j][i] = 1
-
-dx = (1, -1, 0, 0)
-dy = (0, 0, 1, -1)
-
-def fire_bfs():
-    while fire_q:
-        x, y = fire_q.popleft()
+def bfs_fire(fires):
+    q = deque(fires)
+    while q:
+        x, y = q.popleft()
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < M and 0 <= ny < N:
-                if maze[ny][nx] in ['.', 'J'] and visited_fire[ny][nx] == 0:
-                    fire_q.append((nx, ny))
-                    visited_fire[ny][nx] = visited_fire[y][x] + 1
+            if 0 <= nx < r and 0 <= ny < c:
+                if board[nx][ny] in ['.', 'J'] and visited_fire[nx][ny] == 0:
+                    visited_fire[nx][ny] = visited_fire[x][y] + 1
+                    q.append((nx, ny))
 
-fire_bfs()
 
-def j_bfs():
-    while J_q:
-        x, y = J_q.popleft()
+def bfs_J(x, y):
+    q = deque([(x,y)])
+    while q:
+        x, y = q.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            # 탈출 조건
-            if nx < 0 or nx >= M or ny < 0 or ny >= N:
-                return visited_J[y][x]
-            # 불이 이미 번진 경우
-            if visited_fire[ny][nx] != 0 and visited_fire[ny][nx] <= visited_J[y][x] + 1:
+            if nx < 0 or nx >= r or ny < 0 or ny >= c:
+                return visited_j[x][y] + 1
+            
+            if visited_fire[nx][ny] != 0 and visited_fire[nx][ny] <= visited_j[x][y] + 1:
                 continue
-            if maze[ny][nx] == '.' and visited_J[ny][nx] == 0:
-                J_q.append((nx, ny))
-                visited_J[ny][nx] = visited_J[y][x] + 1
+            
+            if board[nx][ny] == '.' and visited_j[nx][ny] == 0:
+                visited_j[nx][ny] = visited_j[x][y] + 1
+                q.append((nx,ny))
 
-result = j_bfs()
+fires = []
+j_x, j_y = 0, 0
+for i in range(r):
+    for j in range(c):
+        if board[i][j] == 'J':
+            j_x, j_y = i, j
+        if board[i][j] == 'F':
+            fires.append((i,j))
 
-if result:
-    print(result)
+bfs_fire(fires)
+res = bfs_J(j_x,j_y)
+if res:
+    print(res)
 else:
     print("IMPOSSIBLE")
