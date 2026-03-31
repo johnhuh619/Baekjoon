8f3f7bb66dc54fbbd19d60e9daa764e9d317697a
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-03-31 15:25:00 +0900

    [Gold IV] Title: 알고스팟, Time: 152 ms, Memory: 112092 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,29 +1,30 @@
 import heapq
-m, n = map(int, input().split())
-graph = [list(map(int, input().strip())) for _ in range(n)] 
+n, m = map(int, input().split())
+board = [list(map(int, input().strip())) for _ in range(m)]
 
-INF = int(1e9)
-pq = [(0,0,0)]
-cost = [[INF]*m for _ in range(n)]
-cost[0][0] = 0
+def djikstra():
+    INF = int(1e9)
+    pq = [(0,0,0)]
+    costs = [[INF]*(n) for _ in range(m)]
+    costs[0][0] = 0
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
 
-dx = [1, -1, 0, 0]
-dy = [0,0,1,-1]
-
-while pq:
-    c, x, y = heapq.heappop(pq)
+    while pq:
+        cost, x, y = heapq.heappop(pq)
+        if cost > costs[x][y]:
+            continue
+            
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n:
+                nc = cost + board[nx][ny]
+                if nc < costs[nx][ny]:
+                    costs[nx][ny] = nc
+                    heapq.heappush(pq, (nc, nx, ny))
     
-    if c > cost[x][y]:
-        continue
-    
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+    return costs
 
-        if 0 <= nx < n and 0 <= ny < m:
-            nc = graph[nx][ny] + c
-            if nc < cost[nx][ny]:
-                cost[nx][ny] = nc
-                heapq.heappush(pq,(nc, nx, ny))
-
-print(cost[n-1][m-1])
+arr = djikstra()
+print(arr[m-1][n-1])
