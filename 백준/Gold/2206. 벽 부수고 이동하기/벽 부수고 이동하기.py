384c034954f21c8f7df65f76a7db4fdffc2f6cf2
허재원/ (Jaewon Huh) <jaewonheo666@gmail.com>
2026-04-02 19:27:46 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1476 ms, Memory: 278312 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,35 +1,33 @@
 from collections import deque
-import sys
-
 n, m = map(int, input().split())
-
+board = [list(map(int, input().strip())) for _ in range(n)]
 visited = [[[0]*2 for _ in range(m)] for _ in range(n)]
-graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(n)]
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-def bfs(x,y,z):
-    visited[x][y][z] = 1
-    q = deque()
-    q.append((x,y,z))
+def bfs():
+    q = deque([(0,0,0)])
+    visited[0][0][0] = 1
+    
     while q:
-        x, y, z = q.popleft()
-        if x == n-1 and y == m-1:
-            return visited[x][y][z]
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0 > nx or nx >= n or 0> ny or ny >= m:
-                continue
+        x, y, broken = q.popleft()
 
-            if graph[nx][ny] == 1 and z == 0:
-                visited[nx][ny][1] = visited[x][y][0] + 1
-                q.append(((nx,ny,1)))
+        if x == n-1 and y == m-1:
+            return visited[x][y][broken]
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
             
-            elif graph[nx][ny] == 0 and visited[nx][ny][z] == 0:
-                visited[nx][ny][z] = visited[x][y][z] + 1
-                q.append((nx,ny,z))
+            if 0 <= nx < n and 0 <= ny < m:
+                if board[nx][ny] == 0 and visited[nx][ny][broken] == 0:
+                    visited[nx][ny][broken] = visited[x][y][broken] + 1
+                    q.append((nx, ny, broken))
+                
+                elif board[nx][ny] == 1 and broken == 0 and visited[nx][ny][1] == 0:
+                    visited[nx][ny][1] = visited[x][y][broken] + 1
+                    q.append((nx, ny, 1))
     return -1
 
-print(bfs(0,0,0))
+print(bfs())
